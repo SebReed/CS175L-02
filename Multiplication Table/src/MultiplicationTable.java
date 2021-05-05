a277ca84b87bc2b48b168d29f57af3a6783bf431
sebastienreed <sebastienreed@10.71.2.132>
2021-05-04 21:55:04 -0400

    Sorry its late

---
+++ Multiplication Table/src/MultiplicationTable.java
@@ -0,0 +1,14 @@
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+for(int one = 1; one!=11; one++)
+{
+for(int two = 1; two!=11; two++)
+{
+System.out.printf("%5d", (one*two));
+}
+System.out.println();
+}
+
+}
+	}
